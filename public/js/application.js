5421044db242566f6e787dbb15c316fc8ce50c11
John Rauschenberg <john.rauschenberg@gmail.com>
2017-07-07 12:10:50 -0700

    Modified header.

--- public/js/application.js
+++ public/js/application.js
@@ -117,17 +117,20 @@ angular.module('MyApp')
 
     $scope.encodeDecode = function(message, key, method) {
 
-      let resultString = '';
+      var resultString = '';
       if (method === 'code') {
-        for (let i=0; i<message.length; i++) {
+        for (var i=0; i<message.length; i++) {
+          if (message(i) === ' ') resultString += ' ';
           resultString += String.fromCharCode(((message.charCodeAt(i) - 97) + (key.charCodeAt(i) - 97)) % 26 + 97);
+
         }
       }
       else {
-        for (let i=0; i<message.length; i++) {
-          let num = ((message.charCodeAt(i) - 97) - (key.charCodeAt(i) - 97)) % 26 + 97;
+        for (var i=0; i<message.length; i++) {
+          var num = ((message.charCodeAt(i) - 97) - (key.charCodeAt(i) - 97)) % 26 + 97;
           if (num < 97) num += 26;
-          resultString += String.fromCharCode(num);
+          if (message(i) === ' ') resultString += ' ';
+          else resultString += String.fromCharCode(num);
         }
       }
       $scope.translation = resultString;
