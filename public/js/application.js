e9e8b2c59bdb6eb5fbff8ff75ca3cf4ebc9b1085
John Rauschenberg <john.rauschenberg@gmail.com>
2017-08-18 13:06:24 -0700

    Added support for most punctuation.

--- public/js/application.js
+++ public/js/application.js
@@ -118,18 +118,21 @@ angular.module('MyApp')
     $scope.encodeDecode = function(message, key, method) {
 
       var resultString = '';
+      var currentChar;
       if (method === 'code') {
         for (var i=0; i<message.length; i++) {
-          if (message[i] === ' ') resultString += ' ';
+          currentChar = message[i];
+          if (currentChar === ' ' || currentChar === '!' || currentChar === ',' || currentChar === '.' || currentChar === ':' || currentChar === ';') resultString += currentChar;
           else resultString += String.fromCharCode(((message.charCodeAt(i) - 97) + (key.charCodeAt(i) - 97)) % 26 + 97);
 
         }
       }
       else {
-        for (var i=0; i<message.length; i++) {
-          var num = ((message.charCodeAt(i) - 97) - (key.charCodeAt(i) - 97)) % 26 + 97;
+        for (var j=0; j<message.length; j++) {
+          var num = ((message.charCodeAt(j) - 97) - (key.charCodeAt(j) - 97)) % 26 + 97;
           if (num < 97) num += 26;
-          if (message[i] === ' ') resultString += ' ';
+          currentChar = message[j];
+          if (currentChar === ' ' || currentChar === '!' || currentChar === ',' || currentChar === '.' || currentChar === ':' || currentChar === ';') resultString += currentChar;
           else resultString += String.fromCharCode(num);
         }
       }
